7143b2f3030b28043a76dd0ede3d172301509a38
Ohav Eden <ohav88@gmail.com>
2023-02-19 17:08:30 +0200

    score

--- script.js
+++ script.js
@@ -3,44 +3,74 @@ var hole = document.getElementById("hole");
 var character = document.getElementById("character");
 var jumping = 0;
 var counter = 0;
-var text=document.querySelector(".text")
-var score=document.querySelector(".score")
+var text = document.querySelector(".text")
+var score = document.querySelector(".score")
+var liveScore = document.getElementById("live-score")
+var highScore = document.getElementById("high-score")
+highScore.innerHTML = `highest score:${JSON.parse(localStorage.getItem("scores"))}`
 
+liveScore.innerHTML = `score:0`
 hole.addEventListener('animationiteration', () => {
-    var random = -((Math.random()*300)+150);
+    var random = -((Math.random() * 300) + 150);
     hole.style.top = random + "px";
     counter++;
+    liveScore.innerHTML = `score:${counter}`
+    saveToLocalStorage(counter)
 });
-setInterval(function(){
+setInterval(function () {
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    if(jumping==0){
-        character.style.top = (characterTop+3)+"px";
+    if (jumping == 0) {
+        character.style.top = (characterTop + 3) + "px";
     }
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-    var cTop = -(500-characterTop);
-    if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
-        alert("Game over. Score: "+(counter-1));
+    var cTop = -(500 - characterTop);
+    if ((characterTop > 480) || ((blockLeft < 20) && (blockLeft > -50) && ((cTop < holeTop) || (cTop > holeTop + 130)))) {
+        alert("Game over. Score: " + (counter));
         // text.classList.remove("hide")
         // score.innerHTML+=counter-1
         character.style.top = 100 + "px";
-        counter=0;
+        counter = 0;
     }
-},10);
+}, 10);
 
-function jump(){
+function jump() {
     jumping = 1;
     let jumpCount = 0;
-    var jumpInterval = setInterval(function(){
+    var jumpInterval = setInterval(function () {
         var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-        if((characterTop>6)&&(jumpCount<15)){
-            character.style.top = (characterTop-5)+"px";
+        if ((characterTop > 6) && (jumpCount < 15)) {
+            character.style.top = (characterTop - 5) + "px";
         }
-        if(jumpCount>20){
+        if (jumpCount > 20) {
             clearInterval(jumpInterval);
-            jumping=0;
-            jumpCount=0;
+            jumping = 0;
+            jumpCount = 0;
         }
         jumpCount++;
-    },10);
+    }, 10);
 }
+
+
+
+
+function saveToLocalStorage(score) {
+    let scores;
+    if (localStorage.getItem("scores") === null) {
+        scores = [];
+    }
+    else {
+        scores = JSON.parse(localStorage.getItem("scores"));
+        console.log(scores);
+    }
+    console.log(scores)
+    if (score > scores) {
+        scores = score;
+    }
+    localStorage.setItem("scores", JSON.stringify(scores))
+}
+
+
+
+
+
